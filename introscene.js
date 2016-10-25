3b2bc8c32f88b53f4cd66a376974c36b7689c7e0
Nikolaj Karakin <karakin.nikolaj@gmail.com>
2016-10-25 23:02:03 +0300

    splash screen с анимированным элементом

--- introscene.js
+++ introscene.js
@@ -4,20 +4,47 @@
 
 var IntroScene = function(_game) {
     Scene.apply( this, arguments );
+
+    this._internalTimer = 0;
+
 };
 
 IntroScene.prototype = Object.create(Scene.prototype);
 IntroScene.prototype.constructor = IntroScene;
 
 IntroScene.prototype.onDraw = function (context, interp) {
-    context.fillStyle = "#FF00FF";
-    context.fillRect( 0, 0, 10, 100 );
+    context.save();
+    context.fillStyle = "#000000";
+    context.fillRect( 0, 0, this._gameRef._width, this._gameRef._height );
+
+    context.textAlign="center";
+
+    context.fillStyle = "#FFFFFF";
+    context.font="50px Arial";
+
+    context.fillText("The Game of #Zavtra", Math.floor( this._gameRef._width / 2 ), Math.floor( this._gameRef._height / 2 ));
+
+    context.fillStyle = "#9c0000";
+    context.font = "25px Arial";
+    context.fillText("Redux", Math.floor( this._gameRef._width / 2 ) + 200, Math.floor( this._gameRef._height / 2 ) + 25);
+
+    context.fillStyle = "#FFFFFF";
+    context.font = ( Math.floor( Math.cos( this._internalTimer * 0.2 ) * 5 ) + 25 ) + "px Arial";
+    context.fillText("Нажмите Enter для начала игры", Math.floor( this._gameRef._width / 2 ), this._gameRef._height - 35);
+
+
+
+    context.restore();
 };
 
 IntroScene.prototype.onInit = function() {
 };
 
 IntroScene.prototype.onUpdate = function() {
+    if( this._internalTimer + 1 > 10000 ) {
+        this._internalTimer = 0;
+    }
+    this._internalTimer++;
 };
 
 IntroScene.prototype.onEvent = function(event) {
